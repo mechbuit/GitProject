b739c5bb6a2c61ebdcf5f40ea098a90498fe5c20
mechbuit <mech.rajesh01@gmail.com>
2018-06-14 04:16:48 +0530

    Java stack class added

---
+++ src/main/java/demo/JavaStack.java
@@ -0,0 +1,49 @@
+package demo;
+
+import java.util.Enumeration;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+
+public class JavaStack {
+
+	public static void main(String[] args) {
+		
+		/*Stack sc = new Stack();
+		sc.add("rajesh1");
+		sc.add("rajesh2");
+		sc.add("rajesh3");
+		sc.add("rajesh4");
+		sc.add("rajesh5");
+
+
+		sc.push(sc.get(2));
+		
+		Enumeration rm = sc.elements();
+		
+		while(rm.hasMoreElements())
+		{
+			System.out.println(rm.nextElement());
+		}*/
+		
+		/*String str = "abccbac";
+		String pallindrome = "";
+		Stack<Character> ch = new Stack<Character>();
+		
+		for(int i=0;i<str.length();i++)
+		{
+			char c = str.charAt(i);
+			ch.push(c);
+		}
+		while(!ch.empty())
+		{
+			pallindrome = pallindrome+ch.pop();
+		}*/
+		
+		/*Queue<String> pr = new PriorityQueue<String>();
+		 pr.po*/
+		
+
+	}
+
+}
